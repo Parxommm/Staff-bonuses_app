baf51c7dbed7f7171f8c167a4c3a031bb275302c
Eugene <evg.parkhomenko@gmail.com>
2021-11-01 22:13:15 +0300

    Создание шаблона под приложение

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+  const data = [
+    {name: 'Evgany P.', salary: 1200, increase: false, id: 1},
+    {name: 'Igor U.', salary: 1500, increase: true, id: 2},
+    {name: 'Sasha S.', salary: 2000, increase: false, id: 3},
+  ];
+
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
